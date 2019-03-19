00c6b92ddd7d690cbc3a4cafb8149554ae669770
krushndayshmookh <krushndayshmookh@gmail.com>
2019-03-19 22:05:26 +0530

    List of machines

--- public/js/init/homepage.js
+++ public/js/init/homepage.js
@@ -1,5 +1,20 @@
 let machineList, machineDetails, tempChart
 
 document.addEventListener('DOMContentLoaded', function() {
-	
+	// showWait()
+	fetch('http://59.99.238.23/arduino/fs_tornos/vk_machine.php') // List of machines
+		.then(function(response) {
+			return response.json()
+		})
+		.then(function(listmachines) {
+			
+			machineList = new Vue({
+				el: '#machineList',
+				data: {
+					
+					selected: undefined,
+					machines: listmachines.map(machine => machine.ojno).sort()
+				}
+			})
+		})
 })
