99c4724e0caa30c792b8c5ed713cc96724380d7f
krushndayshmookh <krushndayshmookh@gmail.com>
2019-04-04 22:40:06 +0530

    try asyncfy

--- public/js/init/homepage.js
+++ public/js/init/homepage.js
@@ -58,55 +58,109 @@ function viewMachineToggle(machineId) {
 					return 0
 				})
 				// console.log(machineData)
-				machineDetails.status = machineData[0].onoff
+				machineDetails.status = machineData.length ? machineData[0].onoff : 0
+
+				// console.log(`Machine status set to ${machineDetails.status}`)
 			})
 
-		fetch('http://59.99.238.23/arduino/fs_tornos/vk_data.php?oj_no=' + machineId) // Temperature data
+		let timearray = []
+		let temparray = []
+
+		fetch('http://59.99.238.23/arduino/fs_tornos/mac_temp.php?oj_no=' + machineId) // Temperature data
 			.then(function(response) {
 				return response.json()
 			})
-			.then(function(machineData) {
+			.then(function(machineTempData) {
 				tempChart.data.labels = []
 				tempChart.data.datasets[0].data = []
 
-				// machineData = [
-				// 	{ seccd: 41, ojno: 5873, datetime: 1552965446000, temp: -127 },
-				// 	{ seccd: 41, ojno: 5873, datetime: 1552965656000, temp: -127 }
+				// machineTempData = [
+				// 	{ seccd: 41, ojno: 5873, dttime: 1552965446000, temp: -127 },
+				// 	{ seccd: 41, ojno: 5873, dttime: 1552965656000, temp: -127 }
 				// ]
 
-				machineData.sort(function(second1, second2) {
-					if (second1.datetime > second2.datetime) return 1 // ascending
-					if (second1.datetime < second2.datetime) return -1
+				machineTempData.sort(function(second1, second2) {
+					if (second1.dttime > second2.dttime) return 1 // ascending
+					if (second1.dttime < second2.dttime) return -1
 					return 0
 				})
 
 				let lasttemp = 0
-				machineData.forEach(second => {
-// 					tempChart.data.labels.push(second.datetime)
+
+				machineTempData.forEach(second => {
+					// timearray.push(second.dttime)
 					lasttemp = second.temp == -127 ? lasttemp : second.temp // set current temperature value to last temp if it is -127
 					let currpoint = {
-						t: second.datetime,
+						t: second.dttime,
 						y: lasttemp
 					}
+					// temparray.push(lasttemp)
 					tempChart.data.datasets[0].data.push(currpoint)
-// 					tempChart.data.datasets[0].data.push(lasttemp)
+					// tempChart.data.datasets[0].data.push(lasttemp)
 				})
 
 				tempChart.update()
-
-				fetch('http://59.99.238.23/arduino/fs_tornos/mac_job.php?oj_no=' + machineId) // list of jobs
-					.then(function(response) {
-						return response.json()
-					})
-					.then(function(machineData) {
-						machineDetails.jobs = machineData
-							.sort(function(second1, second2) {
-								if (second1.dttime > second2.dttime) return 1 // ascending
-								if (second1.dttime < second2.dttime) return -1
-								return 0
-							})
-							.map(job => moment(job.dttime).format('hh:mm:ss'))
-					})
 			})
+
+		fetch('http://59.99.238.23/arduino/fs_tornos/mac_job.php?oj_no=' + machineId + '&date=' + new moment().format('YYYYMMDD')) // list of jobs
+			.then(function(response) {
+				return response.json()
+			})
+			.then(function(machineJobData) {
+				machineDetails.jobs = machineJobData
+					.sort(function(second1, second2) {
+						if (second1.dttime > second2.dttime) return 1 // ascending
+						if (second1.dttime < second2.dttime) return -1
+						return 0
+					})
+					.map(job => moment(job.dttime).format('hh:mm:ss'))
+				// let jobpoints = []
+				tempChart.data.datasets[1].data = []
+
+				machineJobData.forEach(job => {
+
+					let currpoint = {
+						t: job.dttime,
+						// y:  0
+						// y: temparray[timearray.indexOf(job.dttime)]
+						y: tempChart.data.datasets[0].data[tempChart.data.datasets[0].data.map(datapoint => datapoint.t).indexOf(job.dttime)].y
+
+						// y: timearray.indexOf(job.dttime) != -1 ? temparray[timearray.indexOf(job.dttime)] : 0
+					}
+					tempChart.data.datasets[1].data.push(currpoint)
+				})
+				// tempChart.data.datasets[1].data = jobpoints
+				// console.log(jobpoints)
+				tempChart.update()
+
+			})
+
+		async.parallel(
+			[
+				function(cb) {
+					
+					cb(null, "hi")
+					// return "hi"
+				},
+				function(cb) {
+					
+					cb(null, "hi")
+					// return "hi"
+				},
+				function(cb) {
+					
+					cb(null, "hi")
+					// return "hi"
+				},
+				function(cb) {
+					
+					cb(null, "hi")
+					// return "hi"
+				}
+			],
+			function(err, results) {
+				console.log(results)
+			}
+		)
 	}
 }
